9a93ab917058f5a7d954e468b1b5fdefbc2312d5
GeorgeHliopoulos <hliopoulosgeorgios@gmail.com>
2025-09-29 13:54:54 +0300

    commit
    sync/publish

---
+++ main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import { RoleProvider } from './RoleContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RoleProvider>
+        <App />
+      </RoleProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)

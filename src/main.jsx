fa435a3aac6723dfa24f32591014ccc10c59cab4
GeorgeHliopoulos <hliopoulosgeorgios@gmail.com>
2025-09-30 14:17:12 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import { RoleProvider } from './context/RoleContext';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { RoleProvider } from "./context/RoleContext";
+
+/** Καταχώριση του service worker για FCM/PWA */
+if ("serviceWorker" in navigator) {
+  // καταχωρείται από τη ρίζα του site (public/)
+  navigator.serviceWorker.register("/firebase-messaging-sw.js").catch(() => {});
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <RoleProvider>

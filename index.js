4e11c56613c0d140e244b538d73f674209d099c5
GeorgeHliopoulos <hliopoulosgeorgios@gmail.com>
2025-10-02 11:17:16 +0300

    c

--- index.js
+++
@@ -1,7 +0,0 @@
-export const formatDistance = (km) => `${km.toFixed(1)} km`
-
-export const sampleResponses = [
-  { id: 1, pharmacy: 'Green Cross Pharmacy', distance: 1.2, item: 'Amoxicillin 500mg', holdMins: 60 },
-  { id: 2, pharmacy: 'Athens MedCare', distance: 2.4, item: 'Insulin Pen', holdMins: 58 },
-  { id: 3, pharmacy: 'Violeta Iliopoulou', distance: 0.9, item: 'Ibuprofen 400mg', holdMins: 60 },
-]
